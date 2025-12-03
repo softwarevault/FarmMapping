24b3b5bbc3759dcb5b560c72d57b9b513b00cd94
Raymond O Reilly <raysaddress@gmail.com>
2025-12-03 17:27:59 +0000

    Skeleton code priming maven/docker and dependencies

---
+++ src/test/java/com/ray/farm/farm_mapping/FarmMappingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ray.farm.farm_mapping;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FarmMappingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
